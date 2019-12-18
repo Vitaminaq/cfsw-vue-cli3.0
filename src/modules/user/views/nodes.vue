5ff91ee55c75284c5eb159ce365ea3a1e5ece857
xpf <1109614355@qq.com>
2019-12-18 12:52:17 +0800

    feat: build
    build

--- src/modules/user/views/nodes.vue
+++ src/modules/user/views/nodes.vue
@@ -1,15 +1,13 @@
 <template>
-	<swiper
-		v-if="isShow"
-		:options="swiperOption"
-		ref="mySwiper"
-		@someSwiperEvent="callback"
-	>
+	<swiper v-if="isShow" :options="swiperOption" ref="mySwiper">
 		<swiper-slide v-for="(item1, index) in list" :key="index">
 			<div class="contain">
-				<div v-for="(item2, index1) in item1" :key="index1">
+				<!-- <div v-for="(item2, index1) in item1" :key="index1">
 					{{ item2 }}
-				</div>
+				</div> -->
+				<ruby v-for="(item2, index1) in item1" :key="index1">
+					{{ item2.text }} <rt>{{ item2.py }}</rt></ruby
+				>
 			</div>
 		</swiper-slide>
 		<div class="swiper-pagination" slot="pagination"></div>
@@ -42,7 +40,12 @@ export default {
 	computed: {
 		list() {
 			return Array.from({ length: 15 }, (v, i) =>
-				Array.from({ length: 1000 }, (v1, i1) => `${i}${i1}`)
+				Array.from({ length: 1000 }, (v1, i1) => {
+					return {
+						py: 'long',
+						text: '龙'
+					};
+				})
 			);
 		}
 	},
