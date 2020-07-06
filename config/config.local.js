ce0b2f399be0fecc43e00b512e49910550660728
xpf <1109614355@qq.com>
2020-07-06 10:54:58 +0800

    fix: 修复配置
    修复配置

--- config/config.local.js
+++ config/config.local.js
@@ -2,6 +2,6 @@ module.exports = {
 	ENV: 'local',
 	BASE_PATH: '/',
 	// BASE_API: 'http://129.28.167.2:3005'
-	BASE_API: 'http://192.168.1.102:3005',
+	BASE_API: 'https://www.vitaminaq.cn',
 	VERSION: '1.0.0'
 };
