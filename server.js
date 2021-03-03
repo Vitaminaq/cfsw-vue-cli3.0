504d5545adb4ab2f018f0cc9cf9d583b3ba07ee9
肖鹏飞 <humor.xiao@fowo.com>
2021-03-03 14:10:39 +0800

    feat: 增加router-cache
    增加router-cache

--- server.js
+++ server.js
@@ -1,110 +1,110 @@
 // @ts-check
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const serialize = require('serialize-javascript');
-const ip = require('ip');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const serialize = require("serialize-javascript");
+const ip = require("ip");
 
-const isTest = process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD
+const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
 
 async function createServer(
   root = process.cwd(),
-  isProd = process.env.NODE_ENV === 'production'
+  isProd = process.env.NODE_ENV === "production"
 ) {
-  const resolve = (p) => path.resolve(__dirname, p)
+  const resolve = (p) => path.resolve(__dirname, p);
 
   const indexProd = isProd
-    ? fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
-    : ''
+    ? fs.readFileSync(resolve("dist/client/index.html"), "utf-8")
+    : "";
 
   const manifest = isProd
     ? // @ts-ignore
-      require('./dist/client/ssr-manifest.json')
-    : {}
+      require("./dist/client/ssr-manifest.json")
+    : {};
 
-  const app = express()
+  const app = express();
 
   /**
    * @type {import('vite').ViteDevServer}
    */
-  let vite
+  let vite;
   if (!isProd) {
-    vite = await require('vite').createServer({
+    vite = await require("vite").createServer({
       root,
-      logLevel: isTest ? 'error' : 'info',
+      logLevel: isTest ? "error" : "info",
       server: {
-        middlewareMode: true
-      }
-    })
+        middlewareMode: true,
+      },
+    });
     // use vite's connect instance as middleware
-    app.use(vite.middlewares)
+    app.use(vite.middlewares);
   } else {
-    app.use(require('compression')())
+    app.use(require("compression")());
     app.use(
-      require('serve-static')(resolve('dist/client'), {
+      require("serve-static")(resolve("dist/client"), {
         index: false,
-        'setHeaders': (res) => {
-          res.setHeader('Cache-Control', 'public,max-age=60'); // 1000 * 60 * 60 * 24 * 30
-        }
+        setHeaders: (res) => {
+          res.setHeader("Cache-Control", "public,max-age=60"); // 1000 * 60 * 60 * 24 * 30
+        },
       })
-    )
+    );
+    // 响应拦截
+    const routeCache = require("route-cache");
+    app.use(
+      routeCache.cacheSeconds(60, (req) => {
+        const { v } = req.query;
+        console.log("命中缓存", v && `${req.path}${v}`);
+        return v && `${req.path}${v}`;
+      })
+    );
   }
-
-  // 响应拦截
-  const routeCache = require('route-cache');
-  app.use(
-    routeCache.cacheSeconds(60, (req) => {
-      const { v } = req.query;
-      console.log('命中缓存', req.path, v, v && `${req.path}${v}`);
-      return v && `${req.path}${v}`;
-    })
-  );
-  app.use('*', async (req, res) => {
+  app.use("*", async (req, res) => {
     try {
-      const url = req.originalUrl
-      console.log('当前请求路径', url);
+      const url = req.originalUrl;
 
-      let template, render
+      let template, render;
       if (!isProd) {
         // always read fresh template in dev
-        template = fs.readFileSync(resolve('index.html'), 'utf-8')
-        template = await vite.transformIndexHtml(url, template)
-        render = (await vite.ssrLoadModule('/src/entry-server.ts')).render
+        template = fs.readFileSync(resolve("index.html"), "utf-8");
+        template = await vite.transformIndexHtml(url, template);
+        render = (await vite.ssrLoadModule("/src/entry-server.ts")).render;
       } else {
-        template = indexProd
+        template = indexProd;
         // @ts-ignore
-        render = require('./dist/server/entry-server.js').render
+        render = require("./dist/server/entry-server.js").render;
       }
 
-      const [appHtml, preloadLinks, store] = await render(url, manifest)
+      const [appHtml, preloadLinks, store] = await render(url, manifest);
 
-      const state =  ("<script>window.__INIT_STATE__=" + serialize(store, {isJSON: true}) + "</script>");
+      const state =
+        "<script>window.__INIT_STATE__=" +
+        serialize(store, { isJSON: true }) +
+        "</script>";
 
       const html = template
         .replace(`<!--preload-links-->`, preloadLinks)
         .replace(`<!--app-html-->`, appHtml)
-        .replace(`<!--app-store-->`, state)
+        .replace(`<!--app-store-->`, state);
 
-      console.log('渲染');
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+      res.status(200).set({ "Content-Type": "text/html" }).send(html);
     } catch (e) {
-      vite && vite.ssrFixStacktrace(e)
-      console.log(e.stack)
-      res.status(500).end(e.stack)
+      vite && vite.ssrFixStacktrace(e);
+      console.log(e.stack);
+      res.status(500).end(e.stack);
     }
-  })
+  });
 
-  return { app, vite }
+  return { app, vite };
 }
 
 if (!isTest) {
   createServer().then(({ app }) =>
     app.listen(3000, () => {
-      console.log('http://localhost:3000');
+      console.log("http://localhost:3000");
       console.log(`http://${ip.address()}:3000`);
     })
-  )
+  );
 }
 
 // for test use
-exports.createServer = createServer
+exports.createServer = createServer;
